c62873941727c7146c27b36be14c5d4b663318c6
Basil Yusuf - WSL <basilyusuf1709@gmail.com>
2023-10-28 20:53:20 -0400

    Initial api calls

--- app/api/getAyahByNumber/route.ts
+++ app/api/getAyahByNumber/route.ts
@@ -8,12 +8,12 @@ export async function GET(req : NextApiRequest) {
       const urlString = req.url;
       const url = new URL(urlString);
       const params = new URLSearchParams(url.search);
-      const response = await axios.get(`https://api.alquran.cloud/v1/ayah/${params.get("number")}`);
+      console.log(url);
+      const response = await axios.get(`http://api.alquran.cloud/v1/ayah/${params.get("number")}`);
 
       if (response.status !== 200) {
         return NextResponse.json({ message: "Failed to fetch the verse." }, { status: response.status });
       }
-
       return NextResponse.json(response.data.data, { status: 200 });
     }
     return NextResponse.json({ message: "Failed to fetch the verse." }, { status: 500 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
-import Image from 'next/image'
+"use client"
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
+  const [ayah, setAyah] = useState<any>(null);
+  const [arabic, setArabic] = useState<any>(null);
+  const reloadPage = () => {
+    window.location.reload();
+  }
+  useEffect(() => {
+    // Assuming your API endpoint for the random Ayah is '/api/getRandomAyah'
+    fetch('/api/getRandomAyah')
+      .then((response) => response.json())
+      .then((data) => {
+        setAyah(data);
+      })
+      .catch((error: Error) => {
+        console.error('There was an error fetching the Ayah:', error);
+      });
+    fetch('/api/getAyahByAyahNumber?number=2')
+      .then((response) => response.json())
+      .then((data) => {
+        setArabic(data);
+      })
+      .catch((error: Error) => {
+        console.error('There was an error fetching the Ayah:', error);
+      });
+  }, []); // The empty dependency array ensures this useEffect runs once when the component mounts
+
   return (
     <div>
-      Hello
+      {ayah ? <div>{ayah.text}</div> : <div>Loading...</div>}
+      {arabic ? <div>{arabic.text}</div> : <div>Loading...</div>}
+      <button onClick={reloadPage} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
     </div>
-  )
+  );
 }
